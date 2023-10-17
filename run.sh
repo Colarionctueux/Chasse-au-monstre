188579afd6f94964c0f969b7a6df3ca524a7e53d
ThomasGysemans <support@sciencesky.fr>
2023-10-17 14:45:41 +0200

    Add common JAR for the project (making sure that everyone knows how to include it)

--- run.sh
+++ run.sh
@@ -1,1 +1,6 @@
-javac -d bin src/main/java/fr/univlille/*.java && java -cp bin fr.univlille.Main
+if [[ ! -f "lib/cam-player-api-base.jar" ]] ; then
+  echo "T'as oublié d'ajouter la librairie du GOAAAT !!! (dans 'lib' qui s'appelle 'cam-player-api-base.jar' sur Moodle)"
+  exit 1
+fi
+
+javac -cp lib/*.jar -d bin src/main/java/fr/univlille/*.java && java -cp bin:lib/*.jar fr.univlille.Main
--- src/main/java/fr/univlille/Main.java
+++ src/main/java/fr/univlille/Main.java
@@ -1,7 +1,12 @@
 package fr.univlille;
 
+import fr.univlille.iutinfo.cam.player.hunter.IHunterStrategy;
+
 public class Main {
   public static void main(String[] args) {
+
+    IHunterStrategy i;
+
     System.out.println("Hello");
   }
 }
